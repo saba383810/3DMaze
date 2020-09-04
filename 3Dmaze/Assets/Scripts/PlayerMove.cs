514c52db65243160955d386fc58f048f5e15da13
saba383810 <saba383810@gmail.com>
2020-09-04 22:09:36 +0900

    first commit

---
+++ 3Dmaze/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    GameObject eventsystem;
+    WarpSystem warpSystem;
+    private bool windowflag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eventsystem =  GameObject.Find("EventSystem");
+        warpSystem = eventsystem.GetComponent<WarpSystem>();
+        windowflag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward * 0.1f;
+            if (windowflag == true)
+            {
+                windowflag = false;
+                warpSystem.CloseWindow();
+            }
+
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= transform.forward * 0.1f;
+            if (windowflag == true)
+            {
+                windowflag = false;
+                warpSystem.CloseWindow();
+            }
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -3, 0);
+            if (windowflag == true)
+            {
+                windowflag = false;
+                warpSystem.CloseWindow();
+            }
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 3, 0);
+            if (windowflag == true)
+            {
+                windowflag = false;
+                warpSystem.CloseWindow();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (windowflag == true)
+            {
+                transform.position = new Vector3(4, 0, 5);
+                transform.rotation = new Quaternion(0, 180, 0, 0);
+                warpSystem.CloseWindow();
+                windowflag = false;
+            }
+            else
+            {
+                warpSystem.SetWindow();
+                windowflag = true;
+            }
+        }
+    }
+}
---
+++ 3Dmaze/Assets/Scripts/WarpSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpSystem : MonoBehaviour
+{
+    public GameObject window1;
+    public GameObject window2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CloseWindow();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetWindow()
+    {
+        window1.SetActive(true);
+        window2.SetActive(true);
+    }
+
+    public void CloseWindow()
+    {
+        window1.SetActive(false);
+        window2.SetActive(false);
+    }
+}
